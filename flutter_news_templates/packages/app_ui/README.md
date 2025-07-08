ac3fddee4a4559acdf8f9d799db749027df674b3
Faysal Ahmed <19101043@uap-bd.edu>
2025-07-08 23:41:30 +0600

    an application that similar as online news paper

---
+++ flutter_news_templates/packages/app_ui/README.md
@@ -0,0 +1,3 @@
+# Flutter News Templates UI
+
+A UI Toolkit for Flutter News
---
+++ flutter_news_templates/packages/in_app_purchase_repository/lib/in_app_purchase_repository.dart
@@ -0,0 +1,3 @@
+export 'package:flutter_news_templates_api/client.dart'
+    show Subscription, SubscriptionCost, SubscriptionPlan;
+export 'src/in_app_purchase_repository.dart';
